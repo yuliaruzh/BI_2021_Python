b1f15d5bd937ec8f10a2a07af43feb06e826b889
yuliaruzh <yuliaruzh@gmail.com>
2021-09-21 20:20:50 +0300

    Created calculator

---
+++ calculator.py
@@ -0,0 +1,13 @@
+a = float(input())
+c = input()
+b = float(input())
+if c == '+':
+    print(a + b)
+elif c == '-':
+    print(a - b)
+elif c == '*':
+    print(a * b)
+elif c == '/':
+    print(a / b)
+elif c == 'mod':
+    print(a % b)
