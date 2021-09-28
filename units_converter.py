23b27c7f0b68d990aad6718ac6b20eb899e18321
yuliaruzh <yuliaruzh@gmail.com>
2021-09-28 22:12:13 +0300

    fixed mistakes in calculator and weight converter according to flake8

--- units_converter.py
+++ units_converter.py
@@ -1,5 +1,5 @@
-print('Weight converter for units including kilograms(kg), grams(g), stones(st), pounds(lb), '
-      'ounces(oz), drams(dr) and grains(gr)')
+print('Weight converter for units including kilograms(kg), grams(g),'
+      ' stones(st), pounds(lb), ounces(oz), drams(dr) and grains(gr)')
 
 in_weight = float(input('Enter the weight:'))
 in_units = input('Enter the units to convert from:')
@@ -7,7 +7,15 @@ in_convert_units = input('Enter the units to convert to:')
 
 
 def weight_convert(weight, units, convert_units):
-    units_dict = {'kg': 1000, 'g': 1, 'st': 6350.2932, 'lb': 453.5924, 'oz': 28.3495, 'dr': 1.7718, 'gr': 0.0648}
+    units_dict = {
+        'kg': 1000,
+        'g': 1,
+        'st': 6350.2932,
+        'lb': 453.5924,
+        'oz': 28.3495,
+        'dr': 1.7718,
+        'gr': 0.0648
+    }
     return weight * units_dict[units] / units_dict[convert_units]
 
 
